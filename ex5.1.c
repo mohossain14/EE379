47a620cb788105c2a6ac1ef7b4967a4ed47c0751
Mohammad A. Hossain <mo.hossain14@outlook.com>
2022-02-17 21:09:08 -0500

    changes

---
+++ ex5.1.c
@@ -0,0 +1,19 @@
+//prints decimal to binary bit
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int dig, sub, i, k;
+    int max;
+
+    printf("Enter a number: ");
+    scanf("%d", &dig);
+
+    for (i = 0; i < 200; i++){
+        if (pow(2, i) > dig){
+            max = pow(2, i );
+        }
+    }
+    k = 
+}
---
+++ exercise7.c
@@ -0,0 +1,14 @@
+//appects 5 numbers and gives them in order 
+
+#include <stdio.h>
+
+int main(void){
+    int n1, n2, n3, n4, n5;
+
+    printf("Enter 5 numbers with a space between each number: ");
+    scanf("%d %d %d %d %d", &n1, &n2, &n3, &n4, &n5);
+
+    
+
+    return 0;
+}
