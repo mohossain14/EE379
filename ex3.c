4c13f8244a9cae9e6a844a44dee1ef725e805dc3
Mohammad A. Hossain <mo.hossain14@outlook.com>
2022-03-01 12:01:45 -0500

    Finalchanges

---
+++ ex3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int prob1(char s[50]){
+    int i = 0, j = 0;
+    while(char[i] != '\0'){
+        j++;
+        i++;
+
+        return j;
+    }
+}
+int main(void){
+    char word[] = "Hello";
+    int d;
+
+    d = prob1(char word[]);
+    printf("%d", d);
+    return 0;
+
+
+}
